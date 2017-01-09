e09db772bf2dabff7d65c19e63c40c54ae76473f
Example <AppleBottom@users.noreply.github.com>
2017-01-09 23:34:07 +0100

    Added ability to read and use Bob Shemyakin's list in addition to Chris C.'s.

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # applebot
 
 This is (right now) a script to identify still life[1] articles on the
-LifeWiki[2] with suboptimal glider syntheses compared to Chris C.'s list[2].
+LifeWiki[2] with suboptimal glider syntheses compared to Chris C.'s list[3]
+and Bob Shemyakin's list[4].
 
 1. http://conwaylife.com/wiki/Still_life
 2. http://conwaylife.com/wiki/Main_Page
 3. https://github.com/ceebo/glider_synth/blob/master/still_list.txt
+4. http://conwaylife.com/forums/viewtopic.php?p=39164#p39164
--- applebot.pl
+++ applebot.pl
@@ -36,6 +36,11 @@ our $debug_level	= 0;
 # https://github.com/ceebo/glider_synth/blob/master/still_list.txt
 our $still_list_txt	= "still_list.txt";
 
+# filename for Bob Shemyakin's list; should be copied from
+# http://conwaylife.com/forums/viewtopic.php?p=39164#p39164 (or a later post
+# that contains an updated version.
+our $bob_shemyakin_txt	= "bob_shemyakin.txt";
+
 # category to fetch pages from; suggested values are "Strict still lifes" or
 # "Still lifes".
 our $category		= "Strict still lifes";
@@ -49,6 +54,13 @@ our $password		=> undef;
 ####################################################
 ### NO NEED TO CHANGE ANYTHING BEYOND THIS POINT ###
 ####################################################
+
+# version and name
+our $VERSION	= 0.01;
+our $NAME	= "applebot.pl";
+
+# we may dump debug info, in which case sorted hash keys would be nice.
+$Data::Dumper::Sortkeys = 1;
 
 # autoflush STDOUT
 $OUTPUT_AUTOFLUSH = 1;
@@ -85,6 +97,11 @@ sub MAIN {
     my $starting_time = time;
 
     say "Starting up at ", BRIGHT_WHITE, scalar localtime($starting_time), RESET, ".";
+
+    # make sure Chris C's datafile exist.  We can live without Bob
+    # Shemyakin's, but this one is essential for now.
+    -e $still_list_txt
+        or die "$still_list_txt not found.";
     
     # this hash will hold information on our objects.
     my $objects = {};
@@ -92,7 +109,7 @@ sub MAIN {
     # read Chris C.'s list
     print "Reading Chris C.'s list... ";
     my @ceebo_lines = read_file($still_list_txt);
-    
+
     # process Chris C.'s list
     foreach my $ceebo_line (@ceebo_lines) {
     
@@ -108,7 +125,48 @@ sub MAIN {
 
     }
     
-    say GREEN, "done", RESET;
+    say GREEN, "done", RESET, " (", BRIGHT_WHITE, (scalar @ceebo_lines), RESET, " lines read)";
+    
+    # read Bob Shemyakin's list, if found
+    if(-e $bob_shemyakin_txt) {
+    
+        print "Reading Bob Shemyakin's list... ";
+        my @bob_lines = read_file($bob_shemyakin_txt);
+        
+        # the first line is a header line; ignore that.
+        shift @bob_lines;
+        
+        # Bob Shemyakin does not list apgcodes, so we construct a temporary
+        # helper hash converting object numbers to codes.
+        my %number2code = map {
+            $objects->{$_}->{'number'} => $_
+        } keys %$objects;
+        
+        # process Bob Shemyakin's list
+        foreach my $bob_line (@bob_lines) {
+        
+            # split line into fields
+            my ($overall_number, $bits, $bits_number, $chris_gliders, $bob_gliders, $delta_g) = split /\s+/, $bob_line;
+            
+            # this is the number we're interested in.
+            my $number = "${bits}.${bits_number}";
+            
+            if(exists $number2code{$number}) {
+                # we have an apgcode.
+                $objects->{$number2code{$number}}->{'bob_synthesis'} = $bob_gliders;
+            
+            } else {
+                # this shouldn't happen.
+                warn "Could not identify object from Bob Shemyakin's list: $bob_line";
+            
+            }
+        }
+        
+        say GREEN, "done", RESET, " (", BRIGHT_WHITE, (scalar @bob_lines), RESET, " lines read)";
+    }
+    
+    # debugging: dump our object hash.
+    debug_dump($objects);
     
     # log in to the wiki
     unless($be_anonymous) {
@@ -128,14 +186,14 @@ sub MAIN {
     }
 
     # get list of page titles        
-    print "Getting list of strict still lifes... ";
+    print "Getting list of wiki pages... ";
     my @page_titles = $applebot->get_pages_in_category(
         "Category:$category",
         {
             max	=> 0,	# return all results
         }
     );
-    say GREEN, "done", RESET;
+    say GREEN, "done", RESET, " (", BRIGHT_WHITE, (scalar @page_titles), RESET, " articles found)";
     
     foreach my $page_title (@page_titles) {
     
@@ -196,18 +254,28 @@ sub MAIN {
         if(exists $objects->{$apgcode}->{'wiki_synthesis'} and exists $objects->{$apgcode}->{'ceebo_synthesis'}) {
         
             # extract synthesis counts, for convenience
-            my ($wiki_synthesis, $ceebo_synthesis, $page_title) = 
+            my ($wiki_synthesis, $ceebo_synthesis, $bob_synthesis, $page_title) = 
                 map { 
                     $objects->{$apgcode}->{$_} 
-                } ("wiki_synthesis", "ceebo_synthesis", "page_title");
-
+                } ("wiki_synthesis", "ceebo_synthesis", "bob_synthesis", "page_title");
+                
             # is the object without a listed synthesis on the wiki?
             if(not defined $wiki_synthesis) {
-                say BRIGHT_WHITE, $page_title, RESET, " has no synthesis on the wiki, but a ", BRIGHT_WHITE, $ceebo_synthesis, RESET, " glider synthesis in Chris C.'s list.";
+            
+                if(defined $bob_synthesis and $bob_synthesis < $ceebo_synthesis) {
+                    say BRIGHT_WHITE, $page_title, RESET, " has no synthesis on the wiki, but a ", BRIGHT_WHITE, $ceebo_synthesis, RESET, " glider synthesis in Bob Shemyakin's list.";
+                } else {
+                    say BRIGHT_WHITE, $page_title, RESET, " has no synthesis on the wiki, but a ", BRIGHT_WHITE, $ceebo_synthesis, RESET, " glider synthesis in Chris C.'s list.";
+                }
             
             # or is the wiki synthesis worse than Chris C.'s?
-            } elsif($wiki_synthesis > $ceebo_synthesis) {
-                say BRIGHT_WHITE, $page_title, RESET, " has a ", BRIGHT_WHITE, $wiki_synthesis, RESET, " glider synthesis on the wiki, but a ", BRIGHT_WHITE, $ceebo_synthesis, RESET, " glider synthesis in Chris C.'s list.";
+            } elsif($wiki_synthesis > $ceebo_synthesis or $wiki_synthesis > ($bob_synthesis // 0)) {
+                if(defined $bob_synthesis and $bob_synthesis < $ceebo_synthesis) {
+                    say BRIGHT_WHITE, $page_title, RESET, " has a ", BRIGHT_WHITE, $wiki_synthesis, RESET, " glider synthesis on the wiki, but a ", , BRIGHT_WHITE, $ceebo_synthesis, RESET, " glider synthesis in Bob Shemyakin's list.";
+                } else {
+                    say BRIGHT_WHITE, $page_title, RESET, " has a ", BRIGHT_WHITE, $wiki_synthesis, RESET, " glider synthesis on the wiki, but a ", , BRIGHT_WHITE, $ceebo_synthesis, RESET, " glider synthesis in Chris C.'s list.";
+                }
+            
                 
             }
         
@@ -248,4 +316,31 @@ Options:
 ENDOFUSAGE
 
     exit 1;
+}
+
+# dump some data to our debug dump file
+sub debug_dump {
+
+    # file handle state variable
+    state $DUMPER_FILE;
+    
+    # try to open file, if necessary
+    unless(defined $DUMPER_FILE) {
+    
+        # we actually genuinely don't care if this fails. If it doesn't, the
+        # dump won't get logged, and we'll try to open the file again next
+        # time.  Of course it's not ideal if the dump doesn't get logged,
+        # but what alternative is there, really?
+        open $DUMPER_FILE, ">", "dumper.txt";
+    }
+    
+    # if the file is open, log our dump. If the open failed, the dump will
+    # silently be "dumped", no pun intended.  Since this is just logging
+    # this isn't a big deal, and there's not much we can do anyway.
+    if(defined $DUMPER_FILE) {
+        say $DUMPER_FILE Data::Dumper::Dumper(@_);
+    }
+    
+    # note that the dump file never gets closed explicitely in this entire
+    # script.
 }
